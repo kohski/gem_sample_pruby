2083c133c640f9019063bde4b61d08ec86c054da
木村　幸介 <kosukekimura0528@gmail.com>
2019-04-22 11:39:36 +0900

    test release gem packege along with perfect_ruby chp13

---
+++ lib/gem_sample_pruby.rb
@@ -0,0 +1,9 @@
+require "gem_sample_pruby/version"
+
+module GemSamplePruby
+  # class Error < StandardError; end
+  # # Your code goes here...
+  def self.hello_pruby
+    p "hello pruby"
+  end
+end
